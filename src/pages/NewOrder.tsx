de819b0d552af0fbd0d0ddc902621ca6ed352d15
bug2007 <moumitah2007@gmail.com>
2026-07-21 17:02:02 +0600

    testing a req

--- src/pages/NewOrder.tsx
+++ src/pages/NewOrder.tsx
@@ -1,9 +1,13 @@
 import useTitle from "../hooks/useTitle"
+import { createOrder } from "../util/http"
 
 export default function NewOrder() {
     useTitle('Create New Order')
     
     return (
+        <>
         <p>New order</p>
+        <button onClick={createOrder}>Send</button>
+        </>
     )
 }
